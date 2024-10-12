526290c8baa1982d806fe59f3c776be3c67abf69
Dhruv <dhvbnl@gmail.com>
2024-10-12 14:33:39 -0700

    starter history

---
+++ lib/views/history/history.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class History extends StatelessWidget {
+  const History({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('History'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the history page',
+        ),
+      ),
+    );
+  }
+}
