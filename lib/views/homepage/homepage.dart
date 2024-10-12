6206b02ee06317273cbcb0d0a9483abb7732ce71
Keshav Acharya <keshav.ach@outlook.com>
2024-10-12 15:44:38 -0700

    designed homepage

--- lib/views/homepage/homepage.dart
+++ lib/views/homepage/homepage.dart
@@ -10,36 +10,73 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  String prompt = "a pop of red";
+  
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: const Text('stills'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PlatformIconButton(
-              icon: Icon(
-                Icons.camera_alt_rounded,
-                size: 60,
-              )
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'stills',
+            style: TextStyle(
+              color: Colors.white,
             ),
-            SizedBox(height: 60,),
-        
-            PlatformTextButton(
-              onPressed: () {},
-              color: Colors.black,
-              child: const Text('History'),
-              
-            )
-        
-          ],
+          ),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "today's prompt is...",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0
+                    ),
+                  ),
+                Text(
+                    prompt,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0
+                    ),
+                ),]
+              ),
+      
+              SizedBox(height: 5,),
+      
+              PlatformIconButton(
+                icon: Icon(
+                  Icons.camera_alt_rounded,
+                  size: 100,
+                  color: Colors.white,
+                )
+              ),
+              SizedBox(height: 60,),
+          
+              PlatformTextButton(
+                onPressed: () {
+                  print("hello");
+                },
+                color: Colors.white,
+                child: const Text(
+                  'History',
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+          
+            ],
+          ),
         ),
       ),
     );
